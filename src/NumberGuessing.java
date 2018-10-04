6b6e350c42613374dcb88c007299a563139a8a65
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-04 08:37:34 -0400

    Created Intelij project

---
+++ src/NumberGuessing.java
@@ -0,0 +1,13 @@
+public class NumberGuessing {
+
+    public static void main(String[] args) {
+
+        int pickedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+    }
+
+
+}
