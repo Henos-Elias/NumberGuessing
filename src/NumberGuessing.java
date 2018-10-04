f19be79c0600063bc8ae5b31056fca7b8c510145
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-04 08:54:59 -0400

    Intelij project

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class NumberGuessing {
 
     public static void main(String[] args) {
-
+        //declare
+        Scanner keyboard;
         int pickedNumber;
+
 
         pickedNumber = (int)(Math.random()*100);
 
