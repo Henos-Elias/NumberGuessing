6944ce80b12a08083c47a8f821c19c79c65e4f12
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-05 08:14:42 -0400

    Almost finished

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,16 +1,53 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessing {
 
     public static void main(String[] args) {
-        //declare
         Scanner keyboard;
         int pickedNumber;
-
-
         pickedNumber = (int)(Math.random()*100);
-
         System.out.println(pickedNumber);
+
+
+        System.out.println("Hello and welcome to my number guessing game");
+
+        Scanner inputnum = new Scanner(System.in);
+        int maxnum;
+        maxnum = inputnum.nextInt();
+
+        Random rand = new Random();
+        int number = rand.nextInt(maxnum);
+        int tries = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
+
+        while (win == false){
+
+            System.out.println("guess a number between 1 and "+ maxnum +": ");
+            guess = input.nextInt();
+            tries++;
+
+            if (guess == number){
+                win = true;
+
+            }
+
+            else if(guess < number){
+                System.out.println("Number is too low, try again");
+            }
+
+            else if(guess > number){
+                System.out.println("Number is too high, try again");
+
+            }
+
+        }
+
+        System.out.println("You win!");
+        System.out.println("It took you "+ tries + " tries.");
+
     }
 
 
